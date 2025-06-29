7f400ce37c5ba15f9c65d1960909dd1a4171f70f
Ricardo Gomes <rico00gomes@gmail.com>
2025-06-29 19:06:32 -0300

    fix novo ajuste cors para mobilel

--- src/main/java/com/mindtrack/security/WebCorsConfig.java
+++ src/main/java/com/mindtrack/security/WebCorsConfig.java
@@ -10,7 +10,7 @@ public class WebCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:4200,http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
